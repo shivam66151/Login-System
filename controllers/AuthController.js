e2ee0f1404b9f97839b53695118a8acf39cd12fd
shivam66151 <sharmashivam66151@gmail.com>
2022-03-14 02:23:01 +0530

    1 file commited

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -3,33 +3,34 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
 const register = (req,res,next) => {
-    bcrypt.hash(req.body.password, 10, function(err, hashedpass) {
+    bcrypt.hash(req.body.password, 10, function(err, hashedPass) {
        if(err) {
            res.json({
                error: err
            })
        }
+       let user = new User ({
+        name: req.body.name,
+        username: req.body.username,
+        email: req.body.email,
+        department: req.body.department,
+        password: hashedPass
+    })
+    user.save()
+    .then(user => {
+       res.json({
+           message: "User Added Successfully"
+       }) 
+    })
+    .catch(error => {
+        res.json({
+            message: "Error occured!"
+        })
+    })
+    
     })
 }
 
-let user = new User ({
-    name: req.body.name,
-    username: req.body.username,
-    email: req.body.email,
-    department: req.body.department,
-    password: req.body.password
-})
-user.save()
-.then(user => {
-   res.json({
-       message: "User Added Successfully"
-   }) 
-})
-.catch(error => {
-    res.json({
-        message: "Error occured!"
-    })
-})
 
 module.exports = {
     register
